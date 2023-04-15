dffd2c743bc066048bdae939b7d534a29d0af7a6
gupli <gupli@DESKTOP-1VEJR2M>
2023-04-15 21:17:43 +0300

    Сделал отпраку cookie

--- WebBlog/main/views.py
+++ WebBlog/main/views.py
@@ -6,21 +6,30 @@ def authorization(request):
     return render(request, "authorization.html")
  
 def main(request):
+    
     name = request.GET.get("name")
+    tag = request.GET.get("tag")
     password = request.GET.get("password")
-    context = {'isAdmin': checkAdmin(name,password),'posts': takeAllPost()}
+    context = {'isAdmin': checkAdmin(name,password),'posts': takeAllPost(),'tag':tag}
     return render(request, "main.html",context=context)
 
 def post(request):
     name = request.GET.get("post")
-    context = {'post': "firstNotes",'text':takeTextOfPost('firstNotes')}
+    context = {'post': "firstNotes",'text':takeTextOfPost('firstNotes'),'tegs':takeTegOfPost('firstNotes')}
     return render(request, "post.html",context=context)
 
 def AdminPost(request):
     name = request.GET.get("post")
-    context = {'post': "firstNotes",'text':takeTextOfPost('firstNotes')}
+    check = request.GET.get("delete")
+    if(check == "true"):
+        deltePost(name)
+        
+    context = {'post': "firstNotes",'text':takeTextOfPost('firstNotes'),'tegs':takeTegOfPost('firstNotes')}
     return render(request, "AdminPost.html",context=context)
  
+def makePost(request):
+    context = {'post': "firstNotes",'text':takeTextOfPost('firstNotes'),'tegs':takeTegOfPost('firstNotes')}
+    return render(request, "makePost.html",context=context)
  
 
 
