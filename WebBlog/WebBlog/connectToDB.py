f2ccec44cb2f561231df98b21dd881eab91fdf50
gupli <gupli@DESKTOP-1VEJR2M>
2023-04-20 22:06:25 +0300

    Осталось  сделать сесия и отображения картинок

--- WebBlog/WebBlog/connectToDB.py
+++ WebBlog/WebBlog/connectToDB.py
@@ -1,215 +1,216 @@
 import mysql.connector
 import base64
 
- 
+
 def checkAdmin(login, password):
     try:
         conn = mysql.connector.connect(user='root', password='root',
-                                        host='localhost',
-                                        port=3308, # change to the port number used by MAMP
-                                        database='test')
+                                       host='localhost',
+                                       port=3308,  # change to the port number used by MAMP
+                                       database='test')
         cursor = conn.cursor()
-       
-        cursor.execute("SELECT * FROM `loginData`")
-        data = cursor.fetchall()
-        if(data[0] ==(login, password)):
-            conn.close()
-            return True
+
+        cursor.execute("SELECT primaryKey,login,password,firstName FROM `loginData`")
+        dataAboutAll = cursor.fetchall()
+
+        cursor.execute("SELECT nameAdmin,isAdmin FROM `checkadmin`")
+        dataAboutAdmin = cursor.fetchall()
+
+        idUser = ""
+        name =  ""
+        for checkData in dataAboutAll:
+
+            if (checkData[1] == login and checkData[2] == password):
+                idUser = str(checkData[0])
+                name =  str(checkData[3])
+
+        for data in dataAboutAdmin:
+            if (str(data[0]) == str(idUser)):
+                if (str(data[1]) == "1"):
+                    return 1,name
+                elif (str(data[1]) == "0"):
+                    return 0,name
+
+        return -1,name
 
     except mysql.connector.Error as e:
         print("Error connecting to MySQL: ", e)
+
 
 def takeAllPost():
     try:
         conn = mysql.connector.connect(user='root', password='root',
-                                        host='localhost',
-                                        port=3308, # change to the port number used by MAMP
-                                        database='test')
+                                       host='localhost',
+                                       port=3308,  # change to the port number used by MAMP
+                                       database='test')
         cursor = conn.cursor()
-       
+
         cursor.execute("SELECT name,primaryKey FROM `blog`")
         name = cursor.fetchall()
-       
-       
 
-        
-        
         newList = {}
         for name in name:
             newList[name[0]] = name[1]
-          
- 
-        
 
         return newList
-    
+
     except mysql.connector.Error as e:
         print("Error connecting to MySQL: ", e)
+
 
 def takeIdOfPost():
     try:
         conn = mysql.connector.connect(user='root', password='root',
-                                        host='localhost',
-                                        port=3308, # change to the port number used by MAMP
-                                        database='test')
+                                       host='localhost',
+                                       port=3308,  # change to the port number used by MAMP
+                                       database='test')
         cursor = conn.cursor()
-       
+
         cursor.execute("SELECT primaryKey FROM `blog`")
         primaryKey = cursor.fetchall()
-       
-        primaryKey= ' '.join(map(str, primaryKey))
 
-      
-       
-        for ch in ['(',')','\'',',']:
-            primaryKey = primaryKey.replace(ch,'')
-        
+        primaryKey = ' '.join(map(str, primaryKey))
+
+        for ch in ['(', ')', '\'', ',']:
+            primaryKey = primaryKey.replace(ch, '')
+
         primaryKey = primaryKey.split()
-        
+
         return primaryKey
-    
+
     except mysql.connector.Error as e:
         print("Error connecting to MySQL: ", e)
+
 
 def takeTextOfPost(namePost):
     try:
         conn = mysql.connector.connect(user='root', password='root',
-                                        host='localhost',
-                                        port=3308, # change to the port number used by MAMP
-                                        database='test')
+                                       host='localhost',
+                                       port=3308,  # change to the port number used by MAMP
+                                       database='test')
         cursor = conn.cursor()
-       
+
         cursor.execute("SELECT name,data FROM `blog`")
         text = cursor.fetchall()
 
         for txt in text:
-            if(txt[0]==namePost):
+            if (txt[0] == namePost):
                 return txt[1]
 
         return ""
- 
 
     except mysql.connector.Error as e:
         print("Error connecting to MySQL: ", e)
+
 
 def takeTegToPost(teg):
     try:
         conn = mysql.connector.connect(user='root', password='root',
-                                        host='localhost',
-                                        port=3308, # change to the port number used by MAMP
-                                        database='test')
+                                       host='localhost',
+                                       port=3308,  # change to the port number used by MAMP
+                                       database='test')
         cursor = conn.cursor()
         cursor.execute("SELECT name,tegs FROM `blog`")
         name = cursor.fetchall()
-        
+
         my_dict = dict(name)
         tegs = my_dict.get(teg)
-        if(tegs is None):
+        if (tegs is None):
             return ""
-        
+
         return tegs
-    
 
     except mysql.connector.Error as e:
         print("Error connecting to MySQL: ", e)
+
 
 def takeTegOfPosts(teg):
     try:
         conn = mysql.connector.connect(user='root', password='root',
-                                        host='localhost',
-                                        port=3308, # change to the port number used by MAMP
-                                        database='test')
+                                       host='localhost',
+                                       port=3308,  # change to the port number used by MAMP
+                                       database='test')
         cursor = conn.cursor()
         cursor.execute("SELECT name,primaryKey,tegs FROM `blog`")
         name = cursor.fetchall()
-        
-        
-      
-        
+
         newList = {}
         for name in name:
             allTegs = name[2].split(',')
             for tegs in allTegs:
-                if(tegs.lower() == teg.lower() ):
+                if (tegs.lower() == teg.lower()):
                     newList[name[0]] = name[1]
-                    print(str(name[0]) +"|"+ str(name[1])+"|" + str(name[2]))
-            
+                    print(str(name[0]) + "|" + str(name[1])+"|" + str(name[2]))
 
-        
-           
         return newList
-    
 
     except mysql.connector.Error as e:
         print("Error connecting to MySQL: ", e)
+
 
 def deltePost(namePost):
     try:
         conn = mysql.connector.connect(user='root', password='root',
-                                        host='localhost',
-                                        port=3308, # change to the port number used by MAMP
-                                        database='test')
+                                       host='localhost',
+                                       port=3308,  # change to the port number used by MAMP
+                                       database='test')
         cursor = conn.cursor()
-       
-        cursor.execute("DELETE FROM `blog` WHERE `primaryKey` = "+ str(namePost))
+
+        cursor.execute(
+            "DELETE FROM `blog` WHERE `primaryKey` = " + str(namePost))
         conn.commit()
-    
+
     except mysql.connector.Error as e:
         print("Error connecting to MySQL: ", e)
 
 
-
-def addPost(namePost,dataPost,tegPost,imgPost):
+def addPost(namePost, dataPost, tegPost, imgPost,nameAdmin):
     try:
         conn = mysql.connector.connect(user='root', password='root',
-                                        host='localhost',
-                                        port=3308, # change to the port number used by MAMP
-                                        database='test')
-        
+                                       host='localhost',
+                                       port=3308,  # change to the port number used by MAMP
+                                       database='test')
 
-    
         cursor = conn.cursor()
-       
-        
-        # encoded_image = base64.b64encode(imgPost).decode("utf-8")
-    
-        textForInset = f"INSERT INTO `blog` (`primaryKey`, `name`, `tegs`, `data`, `img`) VALUES (NULL, '{namePost}', '{tegPost}', '{dataPost}', Null);"
+
+
+        with open('main/post_images/' + imgPost.name, 'wb+') as destination:
+            for chunk in imgPost.chunks():
+                destination.write(chunk)
+
+        textForInset = f"INSERT INTO `blog` (`primaryKey`, `name`, `tegs`, `data`, `nameImage`, `nameAdmin`) VALUES (NULL, '{namePost}', '{tegPost}', '{dataPost}', '{imgPost}', '{nameAdmin}');"
         cursor.execute(textForInset)
-       
-       
-        conn.commit()
-    
-    except mysql.connector.Error as e:
-        print("Error connecting to MySQL: ", e)
 
-def updatePost(namePost,dataPost,tegPost,imgPost,id):
+        conn.commit()
+
+    except mysql.connector.Error as e:
+        print("Error connecting to MySQLM: ", e)
+
+
+def updatePost(namePost, dataPost, tegPost, imgPost, id,nameAdmin):
     try:
         conn = mysql.connector.connect(user='root', password='root',
-                                        host='localhost',
-                                        port=3308, # change to the port number used by MAMP
-                                        database='test')
-        
+                                       host='localhost',
+                                       port=3308,  # change to the port number used by MAMP
+                                       database='test')
 
         print(imgPost)
         cursor = conn.cursor()
-        textForInset = f"UPDATE `blog` SET `name` = '{namePost}', `tegs` = '{tegPost}', `data` = '{dataPost}' WHERE `blog`.`primaryKey` = {id};"
-
+        textForInset = f"UPDATE `blog` SET `name` = '{namePost}', `tegs` = '{tegPost}', `data` = '{dataPost}', `nameImage` ='{imgPost}', `nameAdmin`=  `{nameAdmin}` WHERE `blog`.`primaryKey` = {id};"
 
         cursor.execute(textForInset)
         conn.commit()
-    
-    except mysql.connector.Error as e:
-        print("Error connecting to MySQL: ", e)
 
+    except mysql.connector.Error as e:
+        print("Error connecting to MySQLU: ", e)
 
 
 def takeImg(namePost):
     try:
         conn = mysql.connector.connect(user='root', password='root',
-                                        host='localhost',
-                                        port=3308, # change to the port number used by MAMP
-                                        database='test')
-        
+                                       host='localhost',
+                                       port=3308,  # change to the port number used by MAMP
+                                       database='test')
 
         cursor = conn.cursor()
         cursor.execute(f"SELECT name,primaryKey FROM `blog`")
@@ -217,63 +218,53 @@ def takeImg(namePost):
 
         id = ""
         for Name in allName:
-            if(Name[0] == namePost):
-                id = Name[1] 
-           
+            if (Name[0] == namePost):
+                id = Name[1]
 
+        print(id)
+        cursor.execute(
+            f"SELECT nameImage FROM `blog` WHERE primaryKey = {id};")
+        img = cursor.fetchone()[0]
 
-        cursor.execute(f"SELECT img FROM `blog` WHERE primaryKey = {id}")
-        img = cursor.fetchone()[0]    
-
-
-        # image_base64 = base64.b64encode(img).decode('utf-8')
-        
-        # return image_base64
-       
+        print(img)
+        allPath = "main/post_images/" + str(img)
+        return allPath
 
     except mysql.connector.Error as e:
-        print("Error connecting to MySQL: ", e)
+        print("Error connecting to MySQLT: ", e)
 
 
 def takeAllImg():
     try:
         conn = mysql.connector.connect(user='root', password='root',
-                                        host='localhost',
-                                        port=3308, # change to the port number used by MAMP
-                                        database='test')
-        
+                                       host='localhost',
+                                       port=3308,  # change to the port number used by MAMP
+                                       database='test')
 
         cursor = conn.cursor()
-        cursor.execute(f"SELECT img,primaryKey FROM `blog`")
+        cursor.execute(f"SELECT nameImage,primaryKey FROM `blog`")
         allImg = cursor.fetchall()
 
-
         newList = {}
-        for result in allImg:
-            if result[0] is not None:
-                image_base64 = base64.b64encode(result[0]).decode('utf-8')
-                
-     
-                newList[result[1]] = image_base64
-         
+        
+
         return newList
-      
 
     except mysql.connector.Error as e:
         print("Error connecting to MySQL: ", e)
-    
-def checkData(title,data,teg):
+
+
+def checkData(title, data, teg):
     check = False
     for symbol in title:
-        if(not symbol.isalpha() and not symbol.isdigit()):
+        if (not symbol.isalpha() and not symbol.isdigit()):
             check = True
-    
+
     return check
+
 
 def clearTegs(tegs):
     import re
-    newtegs = tegs.replace(" ",'')
+    newtegs = tegs.replace(" ", '')
 
- 
-    return re.sub(',+',",",newtegs)
-    
+    return re.sub(',+', ",", newtegs)
--- WebBlog/main/views.py
+++ WebBlog/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import HttpResponse,render
 from WebBlog.connectToDB import *
-from PIL import Image
+import requests
 
 def authorization(request):
     return render(request, "authorization.html")
@@ -10,29 +10,38 @@ def main(request):
     name = request.GET.get("name",'')
     tag = request.GET.get("tag",'')
     password = request.GET.get("password")
-
     idFromMake = request.GET.get("id",'')
-
 
     titleFromMake =  request.POST.get("title",'')
     textFromMake = request.POST.get("text",'')
     tagFromMake = request.POST.get("tag",'')
-    imgFromMake = request.POST.get("img",'')
+    
+    
+  
 
+    # if request.FILES['img'] is not None:
+    #      imgFromMake = request.FILES['img']
+         
+        
+    # if(idFromMake != "" and titleFromMake != "" and textFromMake != ""):
+    #     print("Update")
+    #     updatePost(titleFromMake,textFromMake,clearTegs(tagFromMake),imgFromMake,idFromMake,checkAdmin(name,password)[1])
+    # elif(titleFromMake != "" and textFromMake != ""):
+    #     print("Make")
+    #     addPost(titleFromMake,textFromMake,clearTegs(tagFromMake),imgFromMake,checkAdmin(name,password)[1])
+   
+  
     
     
     if(checkData(titleFromMake,textFromMake,tagFromMake)):
         context = {'post': titleFromMake,'text':textFromMake,'tegs':tagFromMake,'id':id,}
         return render(request, "makePost.html",context)
-
-    if(idFromMake != "" and titleFromMake != "" and textFromMake != ""):
-        updatePost(titleFromMake,textFromMake,clearTegs(tagFromMake),imgFromMake,idFromMake)
-    elif(titleFromMake != "" and textFromMake != ""):
-        addPost(titleFromMake,textFromMake,clearTegs(tagFromMake),imgFromMake)
-   
     
+    
+
+
+
    
-    print(tag)
     allpost ={}
     if(tag == ""):
         allpost =  takeAllPost()
@@ -41,8 +50,9 @@ def main(request):
     
   
    
-    
-    context = {'isAdmin': checkAdmin(name,password),'posts':  allpost,'primaryKey':takeIdOfPost(),'img':takeAllImg()}
+
+
+    context = {'isAdmin': checkAdmin(name,password)[0],'posts':  allpost,'primaryKey':takeIdOfPost(),'img':takeAllImg()}
     return render(request, "main.html",context=context)
 
 
@@ -60,9 +70,10 @@ def AdminPost(request):
 def makePost(request):
     name = request.GET.get("name")
     id = request.GET.get("id")
+
     
-   
     
+  
     if(name!=""):
         context = {'post': name,'text':takeTextOfPost(name),'tegs':takeTegToPost(name),'id':id,}
     else:
